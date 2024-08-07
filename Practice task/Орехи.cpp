31a2e8c39af2ad266cb2a428cca5931d6d5a4fc1
@MaKc@ @MaKc@ <maksim.antonov2012@yandex.ru>
2024-08-07 05:20:58 +0300

    Добавьте файлы проекта.

---
+++ Practice task/Орехи.cpp
@@ -0,0 +1,18 @@
+﻿//Задача 1. Орехи
+#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	float grams,price;
+	
+	std::cout << "Введите цену продукта за 100 грамм: ";
+	std::cin >> price;
+	std::cout << "\n";
+	std::cout << "Введите сколько хочет взять покупатель: ";
+	std::cin >> grams;
+
+	price *= (grams / 100);
+
+	std::cout << "Цена продукта: " << price << " за " << grams << " грамм продукта." << "\n";
+}
